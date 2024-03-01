37086cfbc878edc7b625536dbf5cdf6f47e50704
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-03-01 11:11:24 +0000

    Updated

--- task3/back-end/api.py
+++ task3/back-end/api.py
@@ -1,14 +1,12 @@
-# Import Flask.
-from flask import Flask 
+from flask import Flask
+from flask_cors import CORS
 
-# This will create a Flask App Instance.
 app = Flask(__name__)
+CORS(app)
 
-#Define Route and View Function.
 @app.route('/api/hello')
 def hello_world():
     return 'Hello, World!'
 
-# This will run Flask
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5252)
